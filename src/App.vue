a1df681d0a2a0810ae1dc20e6c816b723cd233f4
AmfixRPN <Amfix.RPN@outlook.com>
2025-05-07 10:53:53 +0500

    ended

---
+++ src/App.vue
@@ -0,0 +1,81 @@
+<script setup>
+import BottomBlocks from "./components/BottomBlocks.vue"
+import { useItemsStore } from "./stores/ItemsStore.js"
+import {computed} from "vue";
+const store = useItemsStore()
+const computedCheckedUserItems = computed(() => store.checkableUserItems.filter((item) => item.checked === true))
+</script>
+
+<template>
+  <div class="top-blocks">
+    <div class="top-user-items">
+      <!--    <div class="top-items" v-for="item in store.checkedUserItems">-->
+      <!--      {{ item.name }}-->
+      <!--    </div>-->
+      <!--    <span>{{ store.checkedUserItems.length }} / {{ store.maxCheckedUserItemsLength }}</span>-->
+      <!--  </div>-->
+      <!--  <div>-->
+      <!--    {{ store.checkedStoreItem }}-->
+      <div class="top-items" v-for="computedItem in computedCheckedUserItems" :key="computedItem.id">
+        {{ computedItem.name }}
+      </div>
+    </div>
+    <div class="top-store-item-block">
+      <div class="top-store-item" v-if="store.checkedStoreItem">
+        {{ store.checkedStoreItem.name }}
+      </div>
+    </div>
+  </div>
+  <div class="btm-blocks">
+    <BottomBlocks :items="store.checkableUserItems" :state="'user'" :checkedItems="computedCheckedUserItems"/>
+    <BottomBlocks :items="store.checkableStoreItems" :state="'store'"/>
+  </div>
+</template>
+
+<style scoped>
+.btm-blocks {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: end;
+}
+.top-user-items {
+  border: black solid;
+  width: 30vw;
+  height: 5vh;
+  position: relative;
+  margin: 3%;
+  padding: 2%;
+  background-color: greenyellow;
+  display: flex;
+  align-items: center;
+}
+.top-items {
+  border: black solid;
+  position: relative;
+  margin: 2%;
+  padding: 2%;
+  background-color: aqua;
+}
+.top-store-item {
+  border: black solid;
+  position: relative;
+  background-color: aqua;
+}
+.top-store-item-block {
+  border: black solid;
+  width: 5vw;
+  height: 5vh;
+  position: relative;
+  margin: 3%;
+  padding: 2%;
+  background-color: greenyellow;
+  display: flex;
+  align-items: center;
+  left: 44%;
+}
+.top-blocks {
+  display: flex;
+  position: relative;
+}
+</style>
---
+++ src/components/BottomBlocks.vue
@@ -0,0 +1,63 @@
+<script setup>
+import {ref} from "vue";
+
+const { items, state, checkedItems } = defineProps(['items', 'state', 'checkedItems'])
+// function handleClick() {
+//   if (state === 'user') {
+//     console.log(event.currentTarget)
+//   }
+// }
+import { useItemsStore } from "../stores/ItemsStore.js";
+const store = useItemsStore()
+//import { computedCheckedUserItems } from "../App.vue";
+</script>
+
+<template>
+  <div class="btm-block">
+    <div v-for="item in items" :key="item.id" class="btm-item" @click="function handleClick(){
+      if (state === 'user') {
+        if (checkedItems.length >= store.maxCheckedUserItemsLength) {
+          item.checked = false
+        } else {
+          item.checked = !item.checked
+        }
+        //store.checkedUserItems.push(item)
+      } else {
+        if (!store.checkedStoreItem) {
+          store.checkedStoreItem = item
+        } else {
+          if (store.checkedStoreItem.id === item.id) {
+            store.checkedStoreItem = ref(null)
+          } else {
+            store.checkedStoreItem = item
+          }
+        }
+      }
+    }">
+      {{ item.name }}
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.btm-block {
+  border: black solid;
+  width: 40vw;
+  height: 40vh;
+  position: relative;
+  margin: 3%;
+  padding: 2%;
+  background-color: greenyellow;
+  display: flex;
+  flex-wrap: wrap;
+}
+.btm-item {
+  border: black solid;
+  margin: 5%;
+  padding: 4%;
+  background-color: aqua;
+  height: 10%;
+  width: 6%;
+  cursor: pointer;
+}
+</style>
